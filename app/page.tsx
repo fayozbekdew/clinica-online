9be19dd6e8c40a4a944e6d9fd85ace1383dcf2e5
fayozbekdew <fayozbekdev@gmail.com>
2024-09-06 15:28:29 +0500

    add admin export function decryptKey(passkey: string) {

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "@/node_modules/next/image";
 import Link from "@/node_modules/next/link";
 
-export default function Home() {
+export default function Home({searchParams}:SearchParamProps) {
+  const isAdmin = searchParams?.admin === 'true'
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO */}
+      { isAdmin && <PasskeyModal/>}
       <section className="remove-scrollbar container my-auto ">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -16,7 +18,7 @@ export default function Home() {
             className="mb-12 h-10 w-fit"
           />
           <PatientForm />
-          <div className="text-14-regular">
+          <div className="text-14-regular flex justify-between pt-8">
             <p className="justify-items-end text-dark-600 xl:text-left">
               @ 2024 Clinica
             </p>
